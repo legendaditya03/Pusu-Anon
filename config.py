307b895fe950a9d581be0577648290430b96617f
legendaditya03 <147244657+legendaditya03@users.noreply.github.com>
2023-12-04 12:22:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/legendaditya03/Pusu-Anon",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Legend-Adi")
 GIT_TOKEN = getenv(
